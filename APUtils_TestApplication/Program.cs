f95e0c60dce3ef5d0a68fb5d98719006b4321811
Alfie J. Palmer <alfie.palmer@ntlworld.com>
2016-11-15 21:54:33 +0000

    Initial Commit

---
+++ APUtils_TestApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace APUtils_TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aputils.AppInfo.Render();
+            for (int i = 0; i < 100; ++i)
+            {
+                aputils.TextProgressBar.Render("Task1()", i);
+                Thread.Sleep(50);
+            }
+            Console.ReadKey();
+        }
+    }
+}
