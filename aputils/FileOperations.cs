a87ba042ae43faec409f622d23338529937e1d7d
Alfie J. Palmer <alfie.palmer@ntlworld.com>
2016-11-16 10:39:44 +0000

    Various Changes

--- aputils/FileOperations.cs
+++ aputils/FileOperations.cs
@@ -1,7 +1,51 @@
-﻿namespace aputils
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace aputils
 {
-    public class FileOps
+    public static class FileOps
     {
+
+        public static void Copy(this FileInfo src, FileInfo dst, Action<int> progressCB)
+        {
+            const int bufferSize = 1024 * 1024;  // 1MB
+            byte[] buffer = new byte[bufferSize], buffer2 = new byte[bufferSize];
+            bool swap = false;
+            int progress = 0, reportedProgress = 0, read = 0;
+            long len = src.Length;
+            float flen = len;
+            Task writer = null;
+
+            using (var source = src.OpenRead())
+            using (var dest = dst.OpenWrite())
+            {
+                dest.SetLength(source.Length);
+                for (long size = 0; size < len; size += read)
+                {
+                    if ((progress = ((int)((size / flen) * 100))) != reportedProgress)
+                        progressCB(reportedProgress = progress);
+
+                    read = source.Read(swap ? buffer : buffer2, 0, bufferSize);
+                    writer?.Wait();
+                    writer = dest.WriteAsync(swap ? buffer : buffer2, 0, read);
+                    swap = !swap;
+                }
+                writer?.Wait();
+            }
+        }
+
+        public static void Move(this FileInfo src, FileInfo dst, Action<int> progressCB)
+        {
+            Copy(src, dst, progressCB); // Copy File Asynchronously
+            src.Delete();               // Perform delete on source file
+        }
+
+        public static void Delete(this FileInfo src)
+        {
+            src.Delete();               // Perform delete operation
+        }
+        
         public enum DecompressionMethod
         {
             ZIP,
