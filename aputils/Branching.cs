2e054b74f94cc0ad5313e7a5c5263048493b4d71
Alfie J. Palmer <alfie.palmer@ntlworld.com>
2016-11-17 11:28:19 +0000

    Added Multi-Choice Branching

--- aputils/Branching.cs
+++ aputils/Branching.cs
@@ -16,10 +16,19 @@ namespace aputils
             string[] legalTokens = resp.Concat(additional).ToArray();
             string input = string.Empty;
 
+            Console.Write(str + " " + string.Join("/", resp) + " : ");
+
+            int curLeft = Console.CursorLeft;
+            int curTop = Console.CursorTop;
+
+            bool invInputFlag = false;
             do
             {
-                Console.Write(str + " " + string.Join("/", resp) + " : ");
+                if (invInputFlag)
+                    Utils.ClearInvalidSelection(curLeft, curTop);
+                
                 input = Console.ReadLine().Trim();
+                invInputFlag = true;
             }
             while (!legalTokens.Any(input.ToUpper().Equals));
 
@@ -27,6 +36,46 @@ namespace aputils
             if (input.ToUpper() == "N" || input.ToUpper() == "NO") return false;
 
             return false; // theoretically, will never reach here
+        }
+
+        public static void MultiChoice(Dictionary<string, Action> funcs) // todo handle exceptions here
+        {
+            int count = 0;
+            int selection = -1;
+
+            Console.WriteLine("Make a selection:\n");
+
+            count = 0;
+            foreach (KeyValuePair<string, Action> s in funcs)
+            {
+                ++count;
+                Console.WriteLine(" " + count + ". " + s.Key);
+            }
+
+            // Prompt User Input
+            Console.Write("\n> ");
+
+            int curLeft = Console.CursorLeft;
+            int curTop = Console.CursorTop;
+
+            bool invInputFlag = false;
+            do
+            {
+                if (invInputFlag)
+                    Utils.ClearInvalidSelection(curLeft, curTop);
+                
+                string str = Console.ReadLine();
+                Int32.TryParse(str, out selection);
+                invInputFlag = true;
+            } while (selection == 0 || selection > funcs.Count);
+
+            count = 0;
+            foreach (KeyValuePair<string, Action> s in funcs)
+            {
+                ++count;
+                if (selection == count)
+                    s.Value();
+            }
         }
     }
 }
