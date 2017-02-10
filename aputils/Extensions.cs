cf7eb2980e18d2f739ba54b5846507c3a7dfaa54
Alfie J. Palmer <alfie.palmer@ntlworld.com>
2017-02-10 17:00:07 +0000

    Create Extensions.cs

---
+++ aputils/Extensions.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+
+namespace testproj
+{
+    public static class Extensions
+    {
+        public static string RemoveDataTypeSuffix(this object o)
+        {
+            var suffixChars = new List<char> { 'f', 'd', 'l', 'o' };
+            string objStr = o.ToTrimString().ToLower();
+
+            foreach (var suffix in suffixChars)
+            {
+                if (!objStr.EndsWith(suffix.ToString()))
+                    continue;
+
+                objStr = objStr.TrimEnd(suffix);
+                return objStr;
+            }
+            return objStr;
+        }
+
+        public static string ToTrimString(this object o)
+        {
+            return o?.ToString().Trim() ?? "";
+        }
+
+        public static int ToInt(this object o)
+        {
+            int ret;
+            return int.TryParse(o.RemoveDataTypeSuffix(), out ret) ? ret : (int)Math.Floor(ToDouble(o));
+        }
+
+        public static long ToLong(this object o)
+        {
+            long ret;
+            return long.TryParse(o.RemoveDataTypeSuffix(), out ret) ? ret : (long)Math.Floor(ToDouble(o));
+        }
+
+        public static float ToFloat(this object o)
+        {
+            float ret;
+            return float.TryParse(o.RemoveDataTypeSuffix(), out ret) ? ret : float.NaN;
+        }
+
+        public static double ToDouble(this object o)
+        {
+            double ret;
+            return double.TryParse(o.RemoveDataTypeSuffix(), out ret) ? ret : double.NaN;
+        }
+
+        public static decimal ToDecimal(this object o)
+        {
+            decimal ret;
+            return decimal.TryParse(o.RemoveDataTypeSuffix(), out ret) ? ret : decimal.MinValue;
+        }
+
+        public static bool ToBoolean(this object o)
+        {
+            if (o.ToInt() == 0) return false;
+            if (o.ToInt() >= 1) return true;
+
+            bool ret;
+            return bool.TryParse(o.ToTrimString(), out ret) && ret;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
